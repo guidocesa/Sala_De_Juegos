1255fe44a77b46bbeb1c433b47e328a30a809a08
Guido <67983976+guidocesa@users.noreply.github.com>
2022-04-07 21:36:29 -0300

    Primer Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,48 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { JuegosComponent } from './juegos/juegos.component';
+import { Prueba1Component } from './juegos/prueba1/prueba1.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'juegos', component: JuegosComponent , children : [ 
+      
+      {
+        path: 'prueba1', component: Prueba1Component
+      }/*
+      {
+        path: 'juego2', asdasd
+      }
+      */
+     ]
+    },
+    { path: '**', component: NotfoundComponent}
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JuegosComponent } from './juegos/juegos.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { Prueba1Component } from './juegos/prueba1/prueba1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ProfileComponent,
+    NavbarComponent,
+    JuegosComponent,
+    NotfoundComponent,
+    Prueba1Component
   ],
   imports: [
     BrowserModule,
